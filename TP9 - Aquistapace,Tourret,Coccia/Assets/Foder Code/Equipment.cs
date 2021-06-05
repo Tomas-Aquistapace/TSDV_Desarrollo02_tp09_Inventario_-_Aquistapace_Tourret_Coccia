49772526b9b274b1ee62d2dd714883f483cf9bef
jctourret <jctourret@gmail.com>
2021-06-05 13:29:52 -0300

    Added foder code to canibalize (El codigo es mio, no es robado)

---
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Foder Code/Equipment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment",menuName ="Inventory/Equipment")] 
+public class Equipment : Item
+{
+    public EquipmentSlot equipslot;
+    public int armorMod;
+    public int damageMod;
+    public int miningMod;
+    public override void Use()
+    {
+        if (!isInShop)
+        {
+            PlayerManager.instance.Equip(this);
+            RemoveFromInventory();
+        }
+        else
+        {
+            PlayerManager.instance.gold += itemValue;
+            Debug.Log("Should remove.");
+            PlayerManager.instance.showGold.updateGold();
+            RemoveFromInventory();
+        }
+    }
+    public override bool Buy()
+    {
+        if(PlayerManager.instance.gold >= itemValue)
+        {
+            PlayerManager.instance.gold -= itemValue;
+            PlayerManager.instance.showGold.updateGold();
+            Inventory.instance.AddToInv(this);
+            return true;
+        }
+        return false;
+    }
+}
+public enum EquipmentSlot { Head, Chest, Leg, Tool, Weapon, Hands, Feet}
---
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Foder Code/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item",menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    new public string name = "Item Name";
+    public Sprite icon = null;
+    public bool defaultItem = false;
+    public bool isInShop = false;
+    public int itemValue;
+    public virtual void Use()
+    {
+        Debug.Log("Using" + name);
+
+    }
+    public virtual bool Buy()
+    {
+        return false;
+    }
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.RemoveFromInv(this);
+    }
+}
