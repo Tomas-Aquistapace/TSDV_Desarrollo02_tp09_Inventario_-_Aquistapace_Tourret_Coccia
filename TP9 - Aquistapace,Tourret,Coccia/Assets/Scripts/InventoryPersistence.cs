efead65546df6cea96df2d109107a419a55aa9dc
Lautaro Coccia <lautaroauditore@gmail.com>
2021-06-05 18:41:33 -0300

    Added create file function

--- TP9 - Aquistapace,Tourret,Coccia/Assets/Scripts/InventoryPersistence.cs
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Scripts/InventoryPersistence.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 public class InventoryPersistence : MonoBehaviour
 {
+    [SerializeField] int data = 0;
+    [SerializeField] string fileName = "Fichero";
+    string path = Application.dataPath + "/"; //Direccion donde se va a guardar el archivo.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateFile(fileName);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //Crea un archivo binario usando el path + el nombre del archivo a crear.
+    public void CreateFile(string name)
+    {
+        if (!File.Exists(path + name))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path + name, FileMode.Create);
+            formatter.Serialize(stream, "");
+            stream.Close();
+        }
+
     }
 }
