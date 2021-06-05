5a1b4030d6d5a85496663bfc0847a3071aa7540c
Tomas <Aquista35@gmail.com>
2021-06-05 14:50:44 -0300

    Added Scriptable obj items

---
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Scrips/Armor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Armor", menuName = "Inventory/Armor")]
+public class Armor : Item
+{
+    public armorSlot _slot;
+    public int _defense = 0;
+    public string _description;
+
+    public override void Use()
+    {
+        Debug.Log("Armor " + _itemName);
+    }
+}
+
+public enum armorSlot { Helmet, Chest, Gloves, Greaves, Boots }
---
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Scrips/Weapons.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Inventory/Weapon")]
+public class Weapon : Item
+{
+    public weaponType _type;
+    public int _damage = 0;
+    public string _description;
+
+    public override void Use()
+    {
+        Debug.Log("Weapon " + _itemName);
+    }
+}
+
+public enum weaponType { Sword, Axe, Spear, Hammer, Bow, Crossbow }
