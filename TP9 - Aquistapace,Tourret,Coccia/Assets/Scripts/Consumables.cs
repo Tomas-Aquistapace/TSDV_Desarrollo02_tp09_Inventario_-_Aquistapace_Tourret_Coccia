1922cad4c601bda66d5892710d4853f3fbd996fd
Lautaro Coccia <lautaroauditore@gmail.com>
2021-06-05 17:05:02 -0300

    Update UI size & script folder name

---
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Scripts/Consumables.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Consumable", menuName = "Inventory/Consumable")]
+public class Consumable : Item
+{
+    public consumableType _type;
+    public int _health = 0;
+    public int _mana = 0;
+    public string _description;
+
+    public override void Use()
+    {
+        Debug.Log("Consumable " + _itemName);
+    }
+}
+
+public enum consumableType { Potion, Food }
---
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string _itemName = "default";
+    public Sprite _sprite;
+    public int _id;
+
+    public virtual void Use()
+    {
+        Debug.Log("Used "+ _itemName);
+    }
+}
