9f4412bc9582256fa6b25352dd3d511efedb1499
Lautaro Coccia <lautaroauditore@gmail.com>
2021-06-05 21:04:56 -0300

    Added save file function

--- TP9 - Aquistapace,Tourret,Coccia/Assets/Scripts/InventoryPersistence.cs
+++ TP9 - Aquistapace,Tourret,Coccia/Assets/Scripts/InventoryPersistence.cs
@@ -4,13 +4,17 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class InventoryPersistence : MonoBehaviour
 {
     [SerializeField] int data = 0;
-    [SerializeField] string fileName = "Fichero";
-    string path = Application.dataPath + "/"; //Direccion donde se va a guardar el archivo.
+    [SerializeField] ScriptableObject obj;
+    [SerializeField] string fileName = "HOLAAA"; //Nombre estandar de el archivo a crear 
+    [SerializeField] string extension = ".dat"; 
+    string path ;  //Direccion donde se va a guardar el archivo.
 
     // Start is called before the first frame update
     void Start()
     {
-        CreateFile(fileName);
+        path = Application.dataPath + "/";
+        //CreateFile(fileName + extension);
+        SaveFile(fileName + extension);
     }
 
     // Update is called once per frame
@@ -19,7 +23,7 @@ public class InventoryPersistence : MonoBehaviour
         
     }
     //Crea un archivo binario usando el path + el nombre del archivo a crear.
-    public void CreateFile(string name)
+    void CreateFile(string name)
     {
         if (!File.Exists(path + name))
         {
@@ -30,4 +34,16 @@ public class InventoryPersistence : MonoBehaviour
         }
 
     }
+   public void SaveFile(string name)
+   {
+       if (!File.Exists(path + name))
+       {
+           CreateFile(name);
+       }
+       BinaryFormatter formatter = new BinaryFormatter();
+       FileStream stream = new FileStream(path + name, FileMode.Open);
+       formatter.Serialize(stream, data);
+       stream.Close();
+   }
+
 }
